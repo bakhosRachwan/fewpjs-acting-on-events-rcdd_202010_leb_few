96d8c3858fef338b93f107ce4eca77017a2f39b4
Bakhos Rachwan <bakhos.rachwan@gmail.com>
2020-11-17 22:25:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function moveDodgerLeft() {
 
 function moveDodgerRight() {
   let rightNumbers = dodger.style.right.replace("px", "");
-  let right = parseInt(rightNumbers, -10);
+  let right = parseInt(rightNumbers, 0);
  
   if (right < 0) {
     dodger.style.right = `${right + 1}px`;
