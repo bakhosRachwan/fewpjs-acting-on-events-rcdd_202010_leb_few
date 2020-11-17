1bea4b2ccfae2a503d49286a70b225b142bf4973
Bakhos Rachwan <bakhos.rachwan@gmail.com>
2020-11-17 21:36:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,3 +15,16 @@ document.addEventListener("keydown", function(e) {
     moveDodgerLeft();
   }
 });
+function moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+ 
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
