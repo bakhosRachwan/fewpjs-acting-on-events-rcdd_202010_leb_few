898cc3f53c84b055de82f485f30cdb7eda047658
Bakhos Rachwan <bakhos.rachwan@gmail.com>
2020-11-17 21:40:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ document.addEventListener("keydown", function(e) {
   if (e.key === "ArrowLeft") {
     moveDodgerLeft();
   }
-  if (e.key === "ArrowLeft") {
-    moveDodgerLeft();
+  if (e.key === "ArrowRight") {
+    moveDodgerRight();
   }
 });
